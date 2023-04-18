198b3c1708d31d474fab786bff66e2bdcdfa1842
Kyle McDonald <hello@kylemcd.com>
2023-04-18 13:27:34 -0500

    📦 NEW: built action

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,144 @@
 require('./sourcemap-register.js');/******/ (() => { // webpackBootstrap
 /******/ 	var __webpack_modules__ = ({
 
+/***/ 20:
+/***/ (function(__unused_webpack_module, exports, __nccwpck_require__) {
+
+"use strict";
+
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+exports.validateConfigFile = exports.getColumnIdForAction = void 0;
+const os = __importStar(__nccwpck_require__(37));
+const getColumnIdForAction = (githubActionEvent, configFile) => {
+    for (const validEvent of configFile.validEvents) {
+        const matchingEvent = validEvent.events.some(event => event.eventName === githubActionEvent.eventName &&
+            (!event.eventTypes ||
+                !githubActionEvent.eventType ||
+                event.eventTypes.includes(githubActionEvent.eventType)));
+        if (matchingEvent &&
+            validEvent.branches.includes(githubActionEvent.branch)) {
+            return validEvent.columnId;
+        }
+    }
+    return null;
+};
+exports.getColumnIdForAction = getColumnIdForAction;
+const validateConfigFile = (configFile) => {
+    const doMatchingValuesExist = ({ sourceArray, matcherArray }) => {
+        if (!sourceArray || !matcherArray) {
+            return false;
+        }
+        return sourceArray.some(sourceValue => {
+            return matcherArray.includes(sourceValue);
+        });
+    };
+    const areAllMatchesTruthy = (matches) => {
+        return matches.every(match => match);
+    };
+    const matchingEvents = configFile.validEvents.filter((validEvent, index) => {
+        const otherEventsArray = [...configFile.validEvents];
+        otherEventsArray.splice(index, 1);
+        const matchingName = otherEventsArray.filter(otherEvents => {
+            return validEvent.events.some(event => {
+                return otherEvents.events.some(otherEvent => {
+                    return doMatchingValuesExist({
+                        sourceArray: [event.eventName],
+                        matcherArray: [otherEvent.eventName]
+                    });
+                });
+            });
+        });
+        const matchingEventTypes = otherEventsArray.filter(otherEvents => {
+            return otherEvents.events.some(otherEvent => {
+                return validEvent.events.some(event => {
+                    return (!event.eventTypes ||
+                        !(otherEvent === null || otherEvent === void 0 ? void 0 : otherEvent.eventTypes) ||
+                        doMatchingValuesExist({
+                            sourceArray: event.eventTypes,
+                            matcherArray: otherEvent.eventTypes
+                        }));
+                });
+            });
+        });
+        const matchingBranches = otherEventsArray.filter(otherEvents => {
+            return doMatchingValuesExist({
+                sourceArray: validEvent.branches,
+                matcherArray: otherEvents.branches
+            });
+        });
+        const matchingColumnId = otherEventsArray.filter(otherEvents => {
+            return otherEvents.columnId === validEvent.columnId;
+        });
+        const matched = areAllMatchesTruthy([
+            !!matchingName,
+            !!matchingEventTypes,
+            !!matchingBranches,
+            !!matchingColumnId
+        ]);
+        const errorFormatter = (matchingArray) => {
+            return matchingArray
+                .map(matchingItem => {
+                return JSON.stringify(matchingItem);
+            })
+                .join(os.EOL);
+        };
+        if (matched) {
+            const errorMesssageContent = matchingName ||
+                matchingEventTypes ||
+                matchingBranches ||
+                matchingColumnId;
+            const errorMessage = `Duplicate config found:${os.EOL}${errorFormatter(errorMesssageContent)}`;
+            throw new Error(errorMessage);
+        }
+    });
+};
+exports.validateConfigFile = validateConfigFile;
+
+
+/***/ }),
+
+/***/ 250:
+/***/ ((__unused_webpack_module, exports) => {
+
+"use strict";
+
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+exports.getShortcutIdFromBranchName = void 0;
+const getShortcutIdFromBranchName = (branchName, branchPattern) => {
+    const match = branchName.match(branchPattern);
+    if (!match)
+        throw new Error('Branch name does not match pattern.');
+    const SHORTCUT_STORY_ID = match[1];
+    return SHORTCUT_STORY_ID;
+};
+exports.getShortcutIdFromBranchName = getShortcutIdFromBranchName;
+
+
+/***/ }),
+
 /***/ 109:
 /***/ (function(__unused_webpack_module, exports, __nccwpck_require__) {
 
@@ -38,18 +176,65 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", ({ value: true }));
 const core = __importStar(__nccwpck_require__(186));
-const wait_1 = __nccwpck_require__(817);
+const path_1 = __importDefault(__nccwpck_require__(17));
+const fs_1 = __importDefault(__nccwpck_require__(147));
+const util_1 = __importDefault(__nccwpck_require__(837));
+const shortcut_1 = __nccwpck_require__(250);
+const github_events_1 = __nccwpck_require__(20);
+const dotenv = __importStar(__nccwpck_require__(437));
+const readFileAsync = util_1.default.promisify(fs_1.default.readFile);
+dotenv.config();
+// TODO: TEMPORARY, DELETE THIS
+const GITHUB_BRANCH = 'bug/sc-22205-user-browsing-as-guest-user-adds-items-to';
+const BRANCH_PATTERN = /sc-(\d+)/;
+const test_event = {
+    eventName: 'push',
+    branch: 'staging'
+};
 function run() {
     return __awaiter(this, void 0, void 0, function* () {
         try {
-            const ms = core.getInput('milliseconds');
-            core.debug(`Waiting ${ms} milliseconds ...`); // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
-            core.debug(new Date().toTimeString());
-            yield (0, wait_1.wait)(parseInt(ms, 10));
-            core.debug(new Date().toTimeString());
-            core.setOutput('time', new Date().toTimeString());
+            const SHORTCUT_TOKEN = core.getInput('SHORTCUT_TOKEN') || process.env.SHORTCUT_TOKEN;
+            const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN') || process.env.GITHUB_TOKEN;
+            const configuration_file = core.getInput('configuration_file') || process.env.CONFIGURATION_FILE;
+            if (!SHORTCUT_TOKEN)
+                throw new Error('SHORTCUT_TOKEN is required.');
+            if (!GITHUB_TOKEN)
+                throw new Error('GITHUB_TOKEN is required.');
+            if (!configuration_file)
+                throw new Error('configuration_file is required.');
+            const buffer = yield readFileAsync(path_1.default.join(__dirname, configuration_file));
+            const json = JSON.parse(buffer.toString());
+            (0, github_events_1.validateConfigFile)(json);
+            const EVENT_NAME = core.getInput('GITHUB_EVENT_NAME');
+            const BRANCH = core.getInput('GITHUB_REF_NAME');
+            const BRANCH_REF = core.getInput('GITHUB_REF_TYPE');
+            if (EVENT_NAME !== 'push' ||
+                EVENT_NAME !== 'pull_request') {
+                throw new Error('Unsupported action trigger');
+            }
+            if (!BRANCH || BRANCH_REF === 'tag') {
+                throw new Error('Branch not found, or tag was used');
+            }
+            const githubActionEvent = {
+                eventName: EVENT_NAME,
+                branch: BRANCH
+            };
+            const columnId = (0, github_events_1.getColumnIdForAction)(githubActionEvent, json);
+            const shortcutStoryId = (0, shortcut_1.getShortcutIdFromBranchName)(githubActionEvent.branch, BRANCH_PATTERN);
+            console.log(columnId, shortcutStoryId);
+            // const shortcut = new ShortcutClient(SHORTCUT_TOKEN)
+            //
+            // shortcut
+            //   .getCurrentMemberInfo()
+            //   .then(response => console.log(response?.data))
+            //
+            // shortcut.listProjects().then(response => console.log(response?.data))
         }
         catch (error) {
             if (error instanceof Error)
@@ -58,37 +243,6 @@ function run() {
     });
 }
 run();
-
-
-/***/ }),
-
-/***/ 817:
-/***/ (function(__unused_webpack_module, exports) {
-
-"use strict";
-
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-Object.defineProperty(exports, "__esModule", ({ value: true }));
-exports.wait = void 0;
-function wait(milliseconds) {
-    return __awaiter(this, void 0, void 0, function* () {
-        return new Promise(resolve => {
-            if (isNaN(milliseconds)) {
-                throw new Error('milliseconds not a number');
-            }
-            setTimeout(() => resolve('done!'), milliseconds);
-        });
-    });
-}
-exports.wait = wait;
 
 
 /***/ }),
@@ -1854,6 +2008,125 @@ exports.checkBypass = checkBypass;
 
 /***/ }),
 
+/***/ 437:
+/***/ ((module, __unused_webpack_exports, __nccwpck_require__) => {
+
+const fs = __nccwpck_require__(147)
+const path = __nccwpck_require__(17)
+const os = __nccwpck_require__(37)
+const packageJson = __nccwpck_require__(968)
+
+const version = packageJson.version
+
+const LINE = /(?:^|^)\s*(?:export\s+)?([\w.-]+)(?:\s*=\s*?|:\s+?)(\s*'(?:\\'|[^'])*'|\s*"(?:\\"|[^"])*"|\s*`(?:\\`|[^`])*`|[^#\r\n]+)?\s*(?:#.*)?(?:$|$)/mg
+
+// Parser src into an Object
+function parse (src) {
+  const obj = {}
+
+  // Convert buffer to string
+  let lines = src.toString()
+
+  // Convert line breaks to same format
+  lines = lines.replace(/\r\n?/mg, '\n')
+
+  let match
+  while ((match = LINE.exec(lines)) != null) {
+    const key = match[1]
+
+    // Default undefined or null to empty string
+    let value = (match[2] || '')
+
+    // Remove whitespace
+    value = value.trim()
+
+    // Check if double quoted
+    const maybeQuote = value[0]
+
+    // Remove surrounding quotes
+    value = value.replace(/^(['"`])([\s\S]*)\1$/mg, '$2')
+
+    // Expand newlines if double quoted
+    if (maybeQuote === '"') {
+      value = value.replace(/\\n/g, '\n')
+      value = value.replace(/\\r/g, '\r')
+    }
+
+    // Add to object
+    obj[key] = value
+  }
+
+  return obj
+}
+
+function _log (message) {
+  console.log(`[dotenv@${version}][DEBUG] ${message}`)
+}
+
+function _resolveHome (envPath) {
+  return envPath[0] === '~' ? path.join(os.homedir(), envPath.slice(1)) : envPath
+}
+
+// Populates process.env from .env file
+function config (options) {
+  let dotenvPath = path.resolve(process.cwd(), '.env')
+  let encoding = 'utf8'
+  const debug = Boolean(options && options.debug)
+  const override = Boolean(options && options.override)
+
+  if (options) {
+    if (options.path != null) {
+      dotenvPath = _resolveHome(options.path)
+    }
+    if (options.encoding != null) {
+      encoding = options.encoding
+    }
+  }
+
+  try {
+    // Specifying an encoding returns a string instead of a buffer
+    const parsed = DotenvModule.parse(fs.readFileSync(dotenvPath, { encoding }))
+
+    Object.keys(parsed).forEach(function (key) {
+      if (!Object.prototype.hasOwnProperty.call(process.env, key)) {
+        process.env[key] = parsed[key]
+      } else {
+        if (override === true) {
+          process.env[key] = parsed[key]
+        }
+
+        if (debug) {
+          if (override === true) {
+            _log(`"${key}" is already defined in \`process.env\` and WAS overwritten`)
+          } else {
+            _log(`"${key}" is already defined in \`process.env\` and was NOT overwritten`)
+          }
+        }
+      }
+    })
+
+    return { parsed }
+  } catch (e) {
+    if (debug) {
+      _log(`Failed to load ${dotenvPath} ${e.message}`)
+    }
+
+    return { error: e }
+  }
+}
+
+const DotenvModule = {
+  config,
+  parse
+}
+
+module.exports.config = DotenvModule.config
+module.exports.parse = DotenvModule.parse
+module.exports = DotenvModule
+
+
+/***/ }),
+
 /***/ 294:
 /***/ ((module, __unused_webpack_exports, __nccwpck_require__) => {
 
@@ -2865,6 +3138,14 @@ module.exports = require("tls");
 
 "use strict";
 module.exports = require("util");
+
+/***/ }),
+
+/***/ 968:
+/***/ ((module) => {
+
+"use strict";
+module.exports = JSON.parse('{"name":"dotenv","version":"16.0.3","description":"Loads environment variables from .env file","main":"lib/main.js","types":"lib/main.d.ts","exports":{".":{"require":"./lib/main.js","types":"./lib/main.d.ts","default":"./lib/main.js"},"./config":"./config.js","./config.js":"./config.js","./lib/env-options":"./lib/env-options.js","./lib/env-options.js":"./lib/env-options.js","./lib/cli-options":"./lib/cli-options.js","./lib/cli-options.js":"./lib/cli-options.js","./package.json":"./package.json"},"scripts":{"dts-check":"tsc --project tests/types/tsconfig.json","lint":"standard","lint-readme":"standard-markdown","pretest":"npm run lint && npm run dts-check","test":"tap tests/*.js --100 -Rspec","prerelease":"npm test","release":"standard-version"},"repository":{"type":"git","url":"git://github.com/motdotla/dotenv.git"},"keywords":["dotenv","env",".env","environment","variables","config","settings"],"readmeFilename":"README.md","license":"BSD-2-Clause","devDependencies":{"@types/node":"^17.0.9","decache":"^4.6.1","dtslint":"^3.7.0","sinon":"^12.0.1","standard":"^16.0.4","standard-markdown":"^7.1.0","standard-version":"^9.3.2","tap":"^15.1.6","tar":"^6.1.11","typescript":"^4.5.4"},"engines":{"node":">=12"}}');
 
 /***/ })
 
